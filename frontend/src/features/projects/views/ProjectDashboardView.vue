2aa12bcbc77c2ff41576cba3092444648dd8af81
BrionesAngel <14tenshikun14@gmail.com>
2026-04-07 20:48:53 -0600

    lucide icons installed

--- frontend/src/features/projects/views/ProjectDashboardView.vue
+++ frontend/src/features/projects/views/ProjectDashboardView.vue
@@ -2,30 +2,18 @@
   <div class="flex flex-col justify-start gap-16 items-center min-h-screen">
     <div class="flex flex-col justify-center items-center w-full gap-8 mt-16">
       <h1 class="text-4xl">DashBoard Overview</h1>
-      <ul class="flex flex-row justify-around w-full">
-        <li>
-          <h3 class="text-2xl">name project</h3>
-          <p>description project</p>
+      <ul class="flex flex-col md:flex-row justify-around w-full">
+        <li v-for="i in 3" :key="i">
+          <PendindgTasksCard class="m-4 md:m-0"></PendindgTasksCard>
         </li>
-
-        <li>
-          <h3 class="text-2xl">name project</h3>
-          <p>description project</p>
-        </li>
-
-        <li>
-          <h3 class="text-2xl">name project</h3>
-          <p>description project</p>
-        </li>
-
       </ul>
     </div>
 
     <div class="flex flex-col justify-center items-center w-full gap-8 mt-16">
       <h1 class="text-4xl">Recent Projects</h1>
-      <ul class="flex flex-row flex-wrap justify-around w-full">
+      <ul class="flex flex-col md:flex-row flex-wrap justify-around w-full">
         <li v-for="i in 4" :key="i">
-          <ProjectCard></ProjectCard>
+          <ProjectCard class="m-4 md:m-0"></ProjectCard>
         </li>
       </ul>
     </div>
@@ -33,6 +21,7 @@
 </template>
 
 <script setup lang="ts">
+import PendindgTasksCard from '@/features/tasks/components/PendindgTasksCard.vue';
 import ProjectCard from '../components/ProjectCard.vue';
 
 
