b7dd504e192e492e96c7460bd6df3facb4cb605e
BrionesAngel <14tenshikun14@gmail.com>
2026-04-10 18:11:33 -0600

    KanbanProjectsCard created & implemented at KanbanView

---
+++ frontend/src/features/kanban/components/KanbanProjectsCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :class="showMembers ? 'bg-white' : 'bg-zinc-100'" class=" flex flex-col justify-center p-4 rounded-md border-0.5 border-indigo-800 shadow-sm
+    shadow-indigo-950 transition-transform duration-200 hover:shadow-md hover:bg-white">
+    <h3 class="text-2xl mb-2">project name</h3>
+    <div class="flex flex-row gap-8 font-medium">
+      <button @click.stop="showMembers = !showMembers"
+        class="flex flex-6 items-center border-2 p-1 rounded-md gap-2 text-md bg-blue-500 hover:bg-blue-900 text-blue-900 hover:text-blue-400 transition">
+        <arrow-up-narrow-wide-icon v-if="showMembers"></arrow-up-narrow-wide-icon>
+        <arrow-down-narrow-wide-icon v-else></arrow-down-narrow-wide-icon>
+        <span class="text-white">members (5)</span>
+      </button>
+      <button
+        class="flex flex-row justify-center items-center gap-2 border-2 px-2 rounded-md text-green-900 bg-green-400 hover:text-green-400 hover:bg-green-900">
+        <users-icon></users-icon>
+        <span class="">all members kanban</span>
+        <square-kanban-icon></square-kanban-icon>
+      </button>
+    </div>
+    <transition name="slide">
+      <div v-if="showMembers" class="border-t mt-3">
+        <ul>
+          <li v-for="i in 5" :key="i" class="flex justify-between items-center border-b p-1">
+            name: name {{ i }}
+            <button
+              class="flex flex-row font-medium items-center border-2 px-2 gap-2 rounded-md bg-yellow-400 hover:bg-yellow-900 text-yellow-900 hover:text-yellow-400">
+              <user-round-icon></user-round-icon>
+              <span>member kanban</span>
+              <sticky-note-icon></sticky-note-icon>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </transition>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { ArrowDownNarrowWideIcon, ArrowUpNarrowWideIcon, SquareKanbanIcon, StickyNoteIcon, UserRoundIcon, UsersIcon, } from '@lucide/vue';
+import { ref } from 'vue';
+
+const showMembers = ref(false)
+</script>
--- frontend/src/features/kanban/views/KanbanView.vue
+++ frontend/src/features/kanban/views/KanbanView.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    kanban view
+    <ul class="w-full md:grid md:grid-cols-3">
+      <li v-for="i in 5" :key="i">
+        <div class="my-8 mx-2" @click="">
+          <KanbanProjectsCard></KanbanProjectsCard>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import KanbanProjectsCard from '../components/KanbanProjectsCard.vue';
+
 
 </script>
