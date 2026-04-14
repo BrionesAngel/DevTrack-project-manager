4ff4065674a9d48db579d7d521f4af635f448908
BrionesAngel <14tenshikun14@gmail.com>
2026-04-13 19:41:17 -0600

    DocsProjectsCard created & added in DocsView

---
+++ frontend/src/features/docs/components/DocsProjectsCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col bg-zinc-100 p-4 rounded-md border-0.5 border-indigo-800 shadow-sm shadow-indigo-950
+      hover:shadow-md
+    gap-2
+      hover:bg-white">
+    <h3 class="text-2xl">project name</h3>
+    <div class="flex flex-row justify-between items-center gap-9 md:gap-4">
+      <button
+        class="flex flex-row justify-center items-center font-medium gap-2 p-1 border-2 px-2 rounded-md text-blue-900 bg-blue-300 hover:text-blue-300 hover:bg-blue-900">
+        <file-pen-line-icon></file-pen-line-icon>
+        <span class="">generate srs</span>
+      </button>
+      <button
+        class="flex flex-row justify-center items-center font-medium gap-2 p-1 border-2 px-2 rounded-md text-green-900 bg-green-300 hover:text-green-300 hover:bg-green-900">
+        <download-icon></download-icon>
+        <span class="">download srs</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { DownloadIcon, FilePenLineIcon } from '@lucide/vue';
+
+
+</script>
--- frontend/src/features/docs/views/DocsView.vue
+++ frontend/src/features/docs/views/DocsView.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    docs view
+    <ul class="w-full md:grid md:grid-cols-2 lg:grid-cols-3">
+      <li v-for="i in 5" :key="i">
+        <div class="my-8 mx-2" @click="">
+          <DocsProjectsCard></DocsProjectsCard>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
+import DocsProjectsCard from '../components/DocsProjectsCard.vue';
+
+
 
 </script>
