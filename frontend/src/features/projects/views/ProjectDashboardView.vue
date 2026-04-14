8b2450ee08b7d200f9e2984864961fc206c8bcaf
BrionesAngel <14tenshikun14@gmail.com>
2026-04-13 19:48:51 -0600

    fixed ui in projectdashboardview

--- frontend/src/features/projects/views/ProjectDashboardView.vue
+++ frontend/src/features/projects/views/ProjectDashboardView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="flex flex-col justify-start gap-1 md:gap-16 items-center min-h-screen">
-    <div class="flex flex-col justify-center items-center w-full gap-8 mt-4 md:mt-16">
+  <div class="flex flex-col justify-start gap-1 md:gap-8 items-center min-h-screen">
+    <div class="flex flex-col justify-center items-center w-full gap-6 mt-4">
       <h1 class="text-4xl">DashBoard Overview</h1>
-      <div class="flex flex-col md:flex-row flex-wrap justify-around w-full gap-4">
+      <div class="flex flex-col md:flex-row flex-wrap justify-center w-full gap-2 md:gap-16">
         <DelayedTasksCard></DelayedTasksCard>
         <RejectedTasksCard></RejectedTasksCard>
         <PendindgTasksCard></PendindgTasksCard>
@@ -10,11 +10,11 @@
       </div>
     </div>
 
-    <div class="flex flex-col justify-center items-center w-full gap-8 mt-4 md:mt-16">
+    <div class="flex flex-col justify-center items-center w-full gap-8 mt-4">
       <div class="flex flex-row justify-center items-center px-4 w-full">
         <h1 class="text-4xl">Recent Projects</h1>
       </div>
-      <ul class="flex flex-col md:flex-row flex-wrap justify-around w-full">
+      <ul class="flex flex-col md:flex-row flex-wrap justify-center w-full">
         <li v-for="i in 4" :key="i">
           <ProjectCard class="m-4"></ProjectCard>
         </li>
