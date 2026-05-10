5cb4ed932e6fbea7bf5e4108c2d1b4ba3eca8ff0
BrionesAngel <14tenshikun14@gmail.com>
2026-05-09 21:14:41 -0600

    login, register views: button colors changed

--- frontend/src/features/auth/views/LoginView.vue
+++ frontend/src/features/auth/views/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="mx-auto flex min-h-screen max-w-md items-center px-4">
-    <div class="w-full rounded-2xl border border-slate-200 bg-white p-8 shadow-lg">
+  <section class="flex mx-auto h-full w-full  items-center justify-center px-4" style="min-height: calc(100vh - 64px)">
+    <div class="w-full max-w-md rounded-2xl border border-slate-200 bg-white p-8 shadow-lg mb-16">
       <h1 class="text-2xl font-bold text-slate-900">Login</h1>
       <p class="mt-1 text-sm text-slate-600">Sign in to manage your app.</p>
 
@@ -8,24 +8,24 @@
         <div>
           <label class="mb-1 block text-sm font-medium text-slate-700" for="email">Email</label>
           <input id="email" v-model="form.email" type="email" required autocomplete="email"
-            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-emerald-300 focus:ring" />
+            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-indigo-400 focus:ring" />
         </div>
 
         <div>
           <label class="mb-1 block text-sm font-medium text-slate-700" for="password">Password</label>
           <input id="password" v-model="form.password" type="password" required autocomplete="current-password"
-            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-emerald-300 focus:ring" />
+            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-indigo-400 focus:ring" />
         </div>
 
         <p v-if="error" class="text-sm text-rose-600">{{ error }}</p>
 
-        <button type="submit" class="w-full rounded-lg bg-emerald-400 px-4 py-2 font-medium text-white hover:bg-emerald-700 cursor-pointer disabled:cursor-not-allowed disabled:opacity-60
+        <button type="submit" class="w-full rounded-lg bg-indigo-900 px-4 py-2 font-medium text-white hover:bg-indigo-950 cursor-pointer disabled:cursor-not-allowed disabled:opacity-60
 " :disabled="authStore.isLoading">
           {{ authStore.isLoading ? 'Signing in...' : 'Sign in' }}
         </button>
         <p class="text-center text-sm text-slate-500">
           Don't have an account?
-          <RouterLink to="/register" class="font-medium text-emerald-400 hover:underline">
+          <RouterLink to="/register" class="font-medium text-indigo-900 hover:underline">
             Register
           </RouterLink>
         </p>
--- frontend/src/features/auth/views/RegisterView.vue
+++ frontend/src/features/auth/views/RegisterView.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="mx-auto flex min-h-screen max-w-md items-center px-4">
-    <div class="w-full rounded-2xl border border-slate-200 bg-white p-8 shadow-lg">
+  <section class="flex mx-auto h-full w-full items-center justify-center px-4" style="min-height: calc(100vh - 64px)">
+    <div class="w-full max-w-md rounded-2xl border border-slate-200 bg-white p-8 shadow-lg mb-16">
       <h1 class="text-2xl font-bold text-slate-900">Register</h1>
       <p class="mt-1 text-sm text-slate-600">Sign up to manage your app.</p>
 
@@ -8,38 +8,38 @@
         <div>
           <label class="mb-1 block text-sm font-medium text-slate-700" for="username">Username</label>
           <input id="username" v-model="form.username" type="text" required autocomplete="username"
-            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-emerald-300 focus:ring" />
+            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-indigo-400 focus:ring" />
         </div>
 
         <div>
           <label class="mb-1 block text-sm font-medium text-slate-700" for="email">Email</label>
           <input id="email" v-model="form.email" type="email" required autocomplete="email"
-            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-emerald-300 focus:ring" />
+            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-indigo-400 focus:ring" />
         </div>
 
         <div>
           <label class="mb-1 block text-sm font-medium text-slate-700" for="password">Password</label>
           <input id="password" v-model="form.password" type="password" required autocomplete="new-password"
-            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-emerald-300 focus:ring" />
+            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-indigo-400 focus:ring" />
         </div>
 
         <div>
           <label class="mb-1 block text-sm font-medium text-slate-700" for="confirmPassword">Confirm Password</label>
           <input id="confirmPassword" v-model="form.confirmPassword" type="password" required
             autocomplete="new-password"
-            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-emerald-300 focus:ring" />
+            class="w-full rounded-lg border border-slate-300 px-3 py-2 outline-none ring-indigo-400 focus:ring" />
         </div>
 
         <p v-if="error" class="text-sm text-rose-600">{{ error }}</p>
 
         <button type="submit"
-          class="w-full rounded-lg bg-emerald-400 px-4 py-2 font-medium text-white hover:bg-emerald-700 disabled:cursor-not-allowed disabled:opacity-60 cursor-pointer"
+          class="w-full rounded-lg bg-indigo-900 px-4 py-2 font-medium text-white hover:bg-indigo-950 disabled:cursor-not-allowed disabled:opacity-60 cursor-pointer"
           :disabled="authStore.isLoading">
           {{ authStore.isLoading ? 'Signing up...' : 'Sign up' }}
         </button>
         <p class="text-center text-sm text-slate-500">
           You have an account?
-          <RouterLink to="/login" class="font-medium text-emerald-400 hover:underline">
+          <RouterLink to="/login" class="font-medium text-indigo-900 hover:underline">
             Sign in
           </RouterLink>
         </p>
