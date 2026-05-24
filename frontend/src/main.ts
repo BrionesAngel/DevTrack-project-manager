4e47cb8da26f8ddbfbafc77e9da485ca1cd6729a
BrionesAngel <14tenshikun14@gmail.com>
2026-05-23 23:11:07 -0600

    vue toast sonner & shadcn added

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,15 @@
 import './assets/main.css'
+import 'vue-sonner/style.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { QueryClient, VueQueryPlugin } from '@tanstack/vue-query'
-
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
-const queryClient = new QueryClient()
-
+export const queryClient = new QueryClient()
 
 app.use(pinia)
 app.use(VueQueryPlugin, { queryClient })
